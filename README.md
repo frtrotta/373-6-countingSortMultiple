21414e368a30e43bdb9023ee3dbd1e3c00a25608
Francesco Trotta <frtrotta@users.noreply.github.com>
2015-04-02 11:12:56 +0200

    To be implemented

---
+++ README.md
@@ -0,0 +1,11 @@
+# 373-6 Counting Sort Multiple
+
+Couting sort of a vector where each element is not unique. For each element
+the algorithms counts the number of the elements in the vector that have a smaller
+value; it then stores this element in a new vector, leaving as many blank spaces as
+the number of smaller elements.
+
+Effettua l'ordinamento di un vettore mediante algoritmo counting-sort: per ogni elemento 
+calcola quanti altri elementi nel vettore hanno valore minore; posiziona, quindi, 
+'elemento corrente in un nuovo vettore lasciando tanti spazi vuoti quanti sono gli 
+elementi calcolati in precedenza. Gli elementi possono essere ripetuti.
---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int countingSortMultiple(int v[], int n, int orderedV[]);
+
+#define N 5
+#define MIN_VALUE -5
+#define MAX_VALUE 40
+
+int main(int argc, char** argv) {
+    int v[N], orderedV[N];
+    int i;
+    
+    srand((unsigned)time(NULL));
+
+    printf("Unordered values\n");
+    for (i = 0; i < N; i++) {
+        v[i] = MIN_VALUE + (rand() % MAX_VALUE) +1;
+        printf("%d: %d\n", i, v[i]);
+    }
+    
+    countingSortMultiple(v, N, orderedV);
+    
+    printf("\n\nOrdered values\n");
+    for (i = 0; i < N; i++) {
+        printf("%d: %d\n", i, orderedV[i]);
+    }
+    
+    return (EXIT_SUCCESS);
+}
+
+int countingSortMultiple(int v[], int n, int orderedV[]) {
+    int r = 0;
+    if(n > 0) {
+        // TODO Implement here
+    }
+    else
+        r = -1;
+    return r;
+}
